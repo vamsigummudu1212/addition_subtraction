f981d6b149da88c42573c51e87b910d010d5171b
vamsigummudu1212 <141009688+vamsigummudu1212@users.noreply.github.com>
2025-01-27 20:43:50 +0530

    Update addition_subtraction.java

--- addition_subtraction.java
+++ addition_subtraction.java
@@ -9,7 +9,7 @@ public class addition_subtraction {
 	public static void main(String args[]) {
 		addition_subtraction R=new addition_subtraction();
 		int x=R.add(56, 34);
-		int y=R.sub(98,9);
+		int y=R.sub(918,98);
 		System.out.println("the total amount was :"+x);
 		System.out.println("the total profit was :"+y);
 		
