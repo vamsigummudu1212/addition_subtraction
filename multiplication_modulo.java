22293c7d6ab75acbbeb1807f4a5b73d317c1cba4
vamsigummudu1212 <141009688+vamsigummudu1212@users.noreply.github.com>
2025-01-27 20:45:01 +0530

    Update multiplication_modulo.java

--- multiplication_modulo.java
+++ multiplication_modulo.java
@@ -11,7 +11,7 @@ public class multiplication_modulo {
 	public static void main(String args []) {
 		multiplication_modulo m=new multiplication_modulo();
 		int q=m.multiply(90,2);
-		int w=m.modulo(90,3 );
+		int w=m.modulo(80,4 );
 		System.out.println("the total interest was :"+q);
 		System.out.println("the rate of interest was :"+w);
 		
